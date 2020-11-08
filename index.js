2b182a199a411c6f9be9fb1f3a42674d153353c8
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2020-11-08 18:53:07 +0000

    added ping (1.0Beta)

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ client.on('ready', () => {
 });
 
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+});
+
 
 client.on("message", message => {
     var content = message.content;
