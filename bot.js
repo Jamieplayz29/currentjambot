88c09472de5815b000485dfa8f84b5b2d5e0a679
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2020-11-11 22:04:18 +0000

    1.0.3

--- bot.js
+++ bot.js
@@ -17,12 +17,17 @@ client.on('message', msg => {
     } 
 }); 
 
-client.on('message', msg => {
-    if (msg.content === `<@434372679668334602>`) {
-        var ping = new Date().getTime() - msg.createdTimestamp
-      msg.reply(`<@434372679668334602>`);
-    } 
-}); 
+client.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+        case 'a':
+            message.channel.send('/a <@434372679668334602>')   
+
+
+    }
+
+})
 
 
 client.on('message', message => {
