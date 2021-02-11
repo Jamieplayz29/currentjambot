df8b932e48a5495d082ac61d177b65142787c557
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-02-11 15:23:55 +0000

    1.0.7

--- bot.js
+++ bot.js
@@ -35,20 +35,6 @@ client.on('message', message => {
 })
 
 
-client.on("message", async (message) => {
-
-    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    // !play Despacito
-    // will play "Despacito" in the member voice channel
-
-    if(command === "play"){
-        client.player.play(message, args[0]);
-        // as we registered the event above, no need to send a success message here
-    }
-
-});
 
 
 
