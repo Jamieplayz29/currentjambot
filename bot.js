decbfd116b0866a5737b59f0202181a68e16bb1d
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-03-05 15:10:33 +0000

    2.0 (beta2)

--- bot.js
+++ bot.js
@@ -89,8 +89,6 @@ client.on("message", async message => {
     const args = message.content.split(" ");
   
     const voiceChannel = message.member.voice.channel;
-
-    try{
       if (!voiceChannel)
       return message.channel.send(
         "join a voice channel then."
@@ -100,8 +98,6 @@ client.on("message", async message => {
       return message.channel.send(
         "my role doesn't let me connect/speak in the channel dud"
       );
-    }} catch (err) {
-      next(err);
     }
   
     const songInfo = await ytdl.getInfo(args[1]);
