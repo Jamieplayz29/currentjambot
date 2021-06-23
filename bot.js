d2465febb465d3a33bee6fb1983b2a190b8bf5ed
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-06-23 09:46:52 +0100

    removed most music modules, added new ones

--- bot.js
+++ bot.js
@@ -58,125 +58,20 @@ client.on("message", message => {
     }
 })
 
-//music
-
-// list of commands to exclude from 'not an actual command'
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    const serverQueue = queue.get(message.guild.id);
-  try {
-    if (message.content.startsWith(`${prefix}play`)) {
-      execute(message, serverQueue);
-      return;
-    } else if (message.content.startsWith(`${prefix}skip`)) {
-      skip(message, serverQueue);
-      return;
-    } else if (message.content.startsWith(`${prefix}stop`)) {
-      stop(message, serverQueue);
-      return;
-    } else if (message.content.startsWith(`${prefix}ping`)) {
-      return;
-    } else {
-      message.channel.send("maybe an actual command would be helpful smh.");
-    }} catch (err) {
-      next(err);
-    }
-  });
-  async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-  
-    const voiceChannel = message.member.voice.channel;
-      if (!voiceChannel)
-      return message.channel.send(
-        "join a voice channel then."
-      );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-      return message.channel.send(
-        "my role doesn't let me connect/speak in the channel dud"
-      );
-    }
-  
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-          title: songInfo.videoDetails.title,
-          url: songInfo.videoDetails.video_url,
-     };
-  
-    if (!serverQueue) {
-      const queueContruct = {
-        textChannel: message.channel,
-        voiceChannel: voiceChannel,
-        connection: null,
-        songs: [],
-        volume: 5,
-        playing: true
-      };
-  
-      queue.set(message.guild.id, queueContruct);
-  
-      queueContruct.songs.push(song);
-  
-      try {
-        var connection = await voiceChannel.join();
-        queueContruct.connection = connection;
-        play(message.guild, queueContruct.songs[0]);
-      } catch (err) {
-        console.log(err);
-        queue.delete(message.guild.id);
-        return message.channel.send(err);
-      }
-    } else {
-      serverQueue.songs.push(song);
-      return message.channel.send(`${song.title} has been added to the queue`);
-    }
-  }
-  
-  function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-      return message.channel.send(
-        "can stop the music if ur not in the channel mate."
-      );
-    if (!serverQueue)
-      return message.channel.send("nothings playing what do u wanna skip...");
-    serverQueue.connection.dispatcher.end();
-  }
-  
-  function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-      return message.channel.send(
-        "join the voice channel to stop the music mate"
-      );
-      
-    if (!serverQueue)
-      return message.channel.send("nothing is playing so theres... nothing to stop LOL!!!");
-      
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-  }
-  
-  function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-      serverQueue.voiceChannel.leave();
-      queue.delete(guild.id);
-      return;
-    }
-  
-    const dispatcher = serverQueue.connection
-      .play(ytdl(song.url))
-      .on("finish", () => {
-        serverQueue.songs.shift();
-        play(guild, serverQueue.songs[0]);
-      })
-      .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-
-  } 
-
-
 
 client.login(token)
+
+
+
+
+
+client.on("message", async (message) => {
+
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "play"){
+        client.player.play(message, args[0]);
+    }
+
+});
