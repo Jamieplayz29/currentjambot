3803632659ae3aecfe911245194720ed671fae53
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-08-09 18:07:44 +0100

    3.1.1

--- bot.js
+++ bot.js
@@ -61,6 +61,18 @@ client.on("message", message => {
     }
 })
 
+client.on("message", message => {
+    let role = message.member.guild.roles.cache.find(role => role.name === 'Mercurial Number Six');
+    if (message.author.id === '498615291908194324') {
+        message.member.roles.add(role);
+        message.delete();
+        console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
+        } else {
+            message.reply('this command is **EXCLUSIVE** to Jam!!');
+            console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
+        };
+}) 
+
 
 
 
--- commands/admin/rolemeup.js
+++ commands/admin/rolemeup.js
@@ -1,16 +1,16 @@
 module.exports =
 {
-    name: 'rolemeup',
-    description: 'adds roles to jam when someone kicks him (me) ',
-    execute(client, message, args) {
-    let role = message.member.guild.roles.cache.find(role => role.name === 'Reddit Admin (eternal virginity)');
-    if (message.author.id === '498615291908194324') {
-        message.member.roles.add(role);
-        message.delete();
-        console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
-        } else {
-            message.reply('this command is **EXCLUSIVE** to Jam!!');
-            console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
-        };
-    }
+    name: '',
+    description: 'addes roles to jam',
+    execute(client,message, args) {
+        let role = message.member.guild.roles.cache.find(role => role.name === 'Mercurial Number Six');
+        if (message.author.id === '498615291908194324') {
+            message.member.roles.add(role);
+            message.delete();
+            console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
+            } else {
+                message.reply('this command is **EXCLUSIVE** to Jam!!');
+                console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
+            };
+    }      
 }
