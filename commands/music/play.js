f8f2e4570b70206aa6a09989ae0b2f5fb5ed59fa
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-09-03 15:17:58 +0100

    4.1.2

--- commands/music/play.js
+++ commands/music/play.js
@@ -2,14 +2,7 @@ module.exports =
 {
     name: 'play',
     description: 'plays a song',
-    async execute(message, args) { 
-        if (!message.member.voice.channelId) return await message.reply({ content: "You are not in a voice channel!", empheral: true });
-        if (message.guild.me.voice.channelId && message.member.voice.channelId !== message.guild.me.voice.channelId) return await message.reply({ content: "You are not in my voice channel!", empheral: true });
-        const query = message.options.get("query").value;
-        const queue = player.createQueue(message.guild, {
-            metadata: {
-                channel: message.channel
-            }
-        });
-    }      
+    execute(message, args) { 
+        message.reply('man cant play music yet')
+    }
 }
