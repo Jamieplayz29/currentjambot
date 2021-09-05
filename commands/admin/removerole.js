e270fbe2a68c284e80b1746a31f5bd791a143c02
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-09-05 19:07:24 +0100

    4.1.3

--- commands/admin/removerole.js
+++ commands/admin/removerole.js
@@ -8,15 +8,26 @@ module.exports =
 
         if(!args[0]) {message.delete(); return};
 
-        let role = message.member.guild.roles.cache.find(role => role.name === args[0]);
-        
-        if (message.author.id === '498615291908194324') {
-            message.member.roles.remove(role);
-            message.delete();
-            console.log(`removed ${role.name} from ${message.author.username} in ${message.guild.name}.`);
-            } else {
-                message.reply('this command is **EXCLUSIVE** to Jam!!');
-                console.log(`${message.author.name} attempted to use the removerole command in ${message.guild.name}`);
+        if(isNaN(args[0])) {
+            let role = message.member.guild.roles.cache.find(role => role.name === args[0]);            
+            if (message.author.id === '498615291908194324') {
+                message.member.roles.remove(role);
+                message.delete();
+                console.log(`removed ${role.name} from ${message.author.username} in ${message.guild.name}.`);
+                } else {
+                    message.reply('this command is **EXCLUSIVE** to Jam!!');
+                    console.log(`${message.author.name} attempted to use the RemoveRole command in ${message.guild.name}`);
             }
+        } else {
+            let role = message.member.guild.roles.cache.find(role => role.id === args[0]);            
+            if(message.author.id === '498615291908194324') {
+                message.member.roles.remove(role);
+                message.delete();
+                console.log(`removed ${role.name} from ${message.author.username} in ${message.guild.name}.`);
+                } else {
+                    message.reply('this command is **EXCLUSIVE** to Jam!!');
+                    console.log(`${message.author.name} attempted to use the RemoveRole command in ${message.guild.name}`);
+                }
+        }    
     }      
 }
---
+++ test.js
@@ -0,0 +1,6 @@
+const value = 'sd'
+
+
+if(isNaN(value)) {
+    console.log('this is not a number')
+} else {console.log('this is a number')}
