154f4df5ba045746b5f09b3c090579e3db7a30f7
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-09-06 18:01:02 +0100

    4.1.6

--- commands/admin/rolemeup.js
+++ commands/admin/rolemeup.js
@@ -20,14 +20,16 @@ module.exports =
             }
         } else {
             let role = message.member.guild.roles.cache.find(role => role.id === args[0]);            
-            if(message.author.id === '498615291908194324') {
-                message.member.roles.add(role);
-                message.delete();
-                console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
-                } else {
-                    message.reply('this command is **EXCLUSIVE** to Jam!!');
-                    console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
-                }
+        if(message.author.id === '498615291908194324') {
+            message.member.roles.add(role);
+            message.delete();
+            console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
+            } else {
+                message.reply('this command is **EXCLUSIVE** to Jam!!');
+                console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
+            }
         }    
     }      
 }
+
+// Mercurial Number Six ID = 694275549304061994
--- commands/admin/smacmedown.js
+++ commands/admin/smacmedown.js
@@ -3,8 +3,11 @@ module.exports =
     name: 'smacmedown',
     description: 'removes all the users roles',
     execute(message, args) {
-        message.member.roles.remove(message.member.roles.cache);
-        message.delete();
-        console.log(`removed all roles from ${message.author.username} in ${message.guild.name}.`);
+        if (message.member.permissions.has('ADMINISTRATOR')) {
+            let member = message.mentions.members.first();
+            member.roles.remove(member.roles.cache);
+            message.channel.send(`removed all roles from ${member} :rofl::rofl::rofl:`)
+            console.log(`removed all roles from ${member} in ${message.guild.name}.`);
+        } else { message.reply('only admins can use the smacmedown command!:sob::sob:') }
     }      
 } 
