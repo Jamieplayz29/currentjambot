bf155e1a2ca36ba53c0a3e10d6724485df60b1c5
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-09-08 22:09:53 +0100

    4.2.2.

--- commands/admin/rolemeup.js
+++ commands/admin/rolemeup.js
@@ -8,28 +8,32 @@ module.exports =
 
         if(!args[0]) {message.delete(); return};
 
-        if(isNaN(args[0])) {
-            let role = message.member.guild.roles.cache.find(role => role.name === args[0]);            
-            if (message.author.id === '498615291908194324') {
+        try {
+            if(isNaN(args[0])) {
+                let role = message.member.guild.roles.cache.find(role => role.name === args[0]);            
+                if (message.author.id === '498615291908194324') {
+                    message.member.roles.add(role);
+                    message.delete();
+                    console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
+                    } else {
+                        message.reply('this command is **EXCLUSIVE** to Jam!!');
+                        console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
+                }
+            } else {
+                let role = message.member.guild.roles.cache.find(role => role.id === args[0]);            
+            if(message.author.id === '498615291908194324') {
                 message.member.roles.add(role);
                 message.delete();
                 console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
                 } else {
                     message.reply('this command is **EXCLUSIVE** to Jam!!');
                     console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
-            }
-        } else {
-            let role = message.member.guild.roles.cache.find(role => role.id === args[0]);            
-        if(message.author.id === '498615291908194324') {
-            message.member.roles.add(role);
-            message.delete();
-            console.log(`added ${role.name} to ${message.author.username} in ${message.guild.name}.`);
-            } else {
-                message.reply('this command is **EXCLUSIVE** to Jam!!');
-                console.log(`${message.author.name} attempted to use the RoleMeUp command in ${message.guild.name}`);
-            }
-        }    
-    }      
+                }
+            }  
+        } catch(error) {
+            console.log(error);
+        }  
+    }   
 }
 
 // Mercurial Number Six ID = 694275549304061994
--- commands/admin/smacmedown.js
+++ commands/admin/smacmedown.js
@@ -1,16 +1,23 @@
+const errorCode = require("rest/interceptor/errorCode");
+
 module.exports =
 {
     name: 'smacmedown',
     description: 'removes all the users roles',
     execute(message, args) {
         if (message.member.permissions.has('ADMINISTRATOR')) {
-            if (!args[0]) {message.reply('you need to ping someone to smac after =smacmedown')}
-            else {
-            let member = message.mentions.members.first();
-            member.roles.remove(member.roles.cache);
-            message.channel.send(`removed all roles from ${member} :rofl::rofl::rofl:`)
-            console.log(`removed all roles from ${member} in ${message.guild.name}.`);
+            let meAndBot = ['<@!709026936919293984>', '<@!498615291908194324>']
+            if(args[0] === '<@!709026936919293984>' ||args[0] === '<@!498615291908194324>') {
+                message.reply('Can\'t remove me or my supreme leader (jam)\'s roles mate');
+            } else {
+                if (!args[0]) {message.reply('you need to ping someone to smac after =smacmedown')}
+                else {
+                let member = message.mentions.members.first();
+                member.roles.remove(member.roles.cache);
+                message.channel.send(`removed all roles from ${member} :rofl::rofl::rofl:`)
+                console.log(`removed all roles from ${member} in ${message.guild.name}.`);
+                }
             }
-        } else { message.reply('only admins can use the smacmedown command!:sob::sob:') }
-    }      
-} 
+        } else { message.reply('only admins can use the smacmedown command!:sob::sob:') }  
+    }
+}
