7ee3a81e47b0681e30e8cb17f4a4153f8848c4cb
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2021-09-11 16:23:54 +0100

    4.2.4

--- commands/admin/dmchannel.js
+++ commands/admin/dmchannel.js
@@ -3,7 +3,7 @@ module.exports =
     name: 'dmchannel',
     description: 'creates an invite link to a server',
     execute(message, args) {
-        if (message.channel.type == 'DMChannel')
+        if (message.channel.type === 'dm')
             console.log(message.channel.type)
     }      
 } 
