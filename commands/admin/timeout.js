76ad73b37e64f7991870afa17543908553f31a35
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2022-01-11 19:28:57 +0000

    small time change

--- commands/admin/timeout.js
+++ commands/admin/timeout.js
@@ -13,7 +13,7 @@ module.exports =
         let time = args[1] * 60 * 100
         let reason = args[2]
         const timeOutEmbed = new MessageEmbed()
-        .setDescription(`**Succesfully timed out ${member} for ${time} minutes \n Reason:** *${reason}*`);
+        .setDescription(`**Succesfully timed out ${member} for ${time / 6000} minutes \n Reason:** *${reason}*`);
         const errorEmbed = new MessageEmbed()
         .setDescription(`An error occured while timeing out ${member} please ensure you have used the correct command format: \n *=timeout [@user] [time in minutes] 'reason for timeout'*`);
 
