8305ddb280108a0434ebb9a108b586df51549533
Jam <66479581+Jamieplayz29@users.noreply.github.com>
2022-10-25 21:19:30 +0100

    Added shush user

--- bot.js
+++ bot.js
@@ -141,8 +141,8 @@ client.on("messageCreate", message => {
 
 client.on("messageCreate", function(message) {
     //telling people to shutup lol little trolling.com XDXD
-    //               Cameron:               Steev:                Leandro:              Mine:
-const userIDs = ['725141738255024229' , '625765223915061289',/*'381177173274263563'*/,/* '498615291908194324' */];
+    //               Cameron:               Steev:             John friend:         Leandro:              Mine:
+const userIDs = ['725141738255024229' , '625765223915061289', '300163171291889664'/*'381177173274263563'*/,/* '498615291908194324' */];
 
 
     const deadResponses = ['Shush please, thanks!!', 'Did i ask', 'If i had a NASA satalite i would use it to try find who asked', 'Sick', 'Ok', 'Omds can u please shush', 'You remember when i asked for your opinion? Nah me neither', 'Cicho bądź', 'Cheeto bądź', 'yoooo thats crazy but who asked']
