95104cfa878fa601d923bb699e8bd754034f7418
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2022-10-27 18:48:24 +0100

    fixed queue

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -7,35 +7,21 @@ module.exports =
     description: 'displays the songs in the queue',
     aliases: ['q'],
     execute(message, args, distube) {
-        const queue = distube.getQueue(message)
-
-
-
-        // embeds
-        const nothingPlaying = new MessageEmbed()
-        .setDescription('Nothing playing right now!');
-
-        if(!queue) return message.channel.send({ embeds: [nothingPlaying]} );
-
-        let embeds = []
-
-        for (i = 0; i < 5; i++)
-        {
-         embeds.push(new Discord.MessageEmbed()
-                .setTitle('Current Queue:')
-                .setDescription(queue.songs
-                                    .map(
-                                        (song, id) =>
-                                            `**${id ? id : 'Playing'}**. ${song.name} - \`${
-                                                song.formattedDuration
-                                            }\``,
-                                )
-                                .slice((i*20), (i+1)*20)
-                                .join('\n')
-                            ))
-        }
-
-    message.channel.createSlider(message.author.id,embeds, "Next", "Back")
-
+		const queue = distube.getQueue(message);
+		if (!queue) {
+			message.channel.send('Nothing playing right now!');
+		} else {
+			message.channel.send(
+				`Current queue:\n${queue.songs
+					.map(
+						(song, id) =>
+							`**${id ? id : 'Playing'}**. ${
+								song.name
+							} - \`${song.formattedDuration}\``,
+					)
+					.slice(0, 10)
+					.join('\n')}`,
+			);
+		}
 	}
 }
