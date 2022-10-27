e9211c07d4f0885bb846ea61957f3076a1a27897
Jamieplayz29 <66479581+Jamieplayz29@users.noreply.github.com>
2022-10-27 18:46:55 +0100

    fixed distube

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,6 +4,14 @@ module.exports =
     description: 'plays a song',
     aliases: ['p'],
     execute( message, args, distube) {
-        distube.play(message, args.join(' '))
+        distube
+		.play(message.member.voice.channel, args.join(' '), {
+			message,
+			textChannel: message.channel,
+			member: message.member,
+		})
+		.catch(err => {
+			message.reply(err.message);
+		});
     }
 } 
