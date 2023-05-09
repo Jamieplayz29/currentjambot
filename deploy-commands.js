fad0931ae104ca5931fdb1978e039da6ab976b91
Jam <66479581+Jamieplayz29@users.noreply.github.com>
2023-05-09 21:06:20 +0100

    changed commands to global

--- deploy-commands.js
+++ deploy-commands.js
@@ -36,7 +36,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 
